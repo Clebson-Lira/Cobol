b810823070f321a2aeb4d184aef404273c986095
Clebson <clebson.lira@essentia.com.br>
2024-12-09 17:10:09 -0300

    Refatoração do programa CAIXA em COBOL: melhorias na estrutura e nomenclatura, adição de novos procedimentos para menu e operações de depósito e saque.

--- CAIXA.cob
+++ CAIXA.cob
@@ -1,65 +1,79 @@
-IDENTIFICATION DIVISION.
-       PROGRAM-ID. CAIXA-ELETRONICO.
-
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. CAIXA.
+       AUTHOR. "Clebson Lira".
        DATA DIVISION.
-       WORKING-STORAGE SECTION.
-       01 SALDO PIC 9(5)V99 VALUE 0.
-       01 OPCOES PIC 9 VALUE 0.
-       01 VALOR PIC 9(5)V99 VALUE 0.
-       01 CONTINUAR PIC X VALUE 'S'.
-
+           WORKING-STORAGE SECTION.
+           77 WS-OPCAO PIC X(2).
+           77 WS-DEPOSITO PIC 9(5)V99 VALUE ZERO.
+           77 WS-SAQUE PIC 9(5)V99 VALUE ZERO.
+           77 WS-SALDO PIC 9(5)V99 VALUE ZERO.
+           77 WS-SALDO-FORMATADO PIC ZZZZZ.99.
+           77 WS-FIM-EXECUCAO PIC X VALUE "N".
        PROCEDURE DIVISION.
-      *> cobol-lint CL002 main-procedure
-       MAIN-PROCEDURE.
-           PERFORM UNTIL CONTINUAR = 'N'
-               DISPLAY 'CAIXA ELETRONICO'
-               DISPLAY '1 - VER SALDO'
-               DISPLAY '2 - DEPOSITAR'
-               DISPLAY '3 - SACAR'
-               DISPLAY '4 - SAIR'
-               ACCEPT OPCOES
-               EVALUATE OPCOES
-                   WHEN 1
-                       PERFORM VER-SALDO
-                   WHEN 2
-                       PERFORM DEPOSITAR
-                   WHEN 3
-                       PERFORM SACAR
-                   WHEN 4
-                       MOVE 'N' TO CONTINUAR
+           MAIN-PROCESS.
+               PERFORM UNTIL WS-FIM-EXECUCAO = "S"
+                   PERFORM P001-MENU1
+               END-PERFORM
+               DISPLAY "OBRIGADO POR USAR O BANCO LIRA. VOLTE SEMPRE!"
+               STOP RUN.
+
+           P001-MENU1.
+               PERFORM P000-TITULO
+               DISPLAY "1 - DEPOSITO".
+               DISPLAY "2 - SAQUE".
+               DISPLAY "3 - SALDO".
+               DISPLAY "4 - SAIR".
+               ACCEPT WS-OPCAO
+               EVALUATE WS-OPCAO
+                   WHEN "1" 
+                       PERFORM P002-DEPOSITO
+                   WHEN "2" 
+                       PERFORM P003-SAQUE
+                   WHEN "3" 
+                       PERFORM P004-SALDO
+                   WHEN "4" 
+                       DISPLAY "SAINDO DO SISTEMA..."
+                       MOVE "S" TO WS-FIM-EXECUCAO
                    WHEN OTHER
-                       DISPLAY 'OPCAO INVALIDA'
-               END-EVALUATE
-           END-PERFORM
-           STOP RUN.
+                       DISPLAY "OPCAO INVALIDA, TENTE NOVAMENTE."
+               END-EVALUATE.
 
-       VER-SALDO.
-           DISPLAY 'SEU SALDO E: ' SALDO.
+           P000-TITULO.
+               DISPLAY "-------------------------------".
+               DISPLAY "-        BANCO LIRA           -".
+               DISPLAY "-------------------------------".
 
-       DEPOSITAR.
-           DISPLAY 'DIGITE O VALOR PARA DEPOSITAR:'
-           ACCEPT VALOR
-           IF VALOR > 0 THEN
-               ADD VALOR TO SALDO
-               DISPLAY 'DEPOSITO REALIZADO COM SUCESSO. SEU NOVO SALDO E: ' SALDO
-           ELSE
-               DISPLAY 'VALOR INVALIDO PARA DEPOSITO.'
-           END-IF.
-
-       SACAR.
-           DISPLAY 'DIGITE O VALOR PARA SACAR:'
-           ACCEPT VALOR
-           IF VALOR > 0 THEN
-               IF VALOR > SALDO THEN
-                   DISPLAY 'SALDO INSUFICIENTE.'
+           P002-DEPOSITO.
+               PERFORM P000-TITULO
+               DISPLAY "VALOR DO DEPOSITO: "
+               ACCEPT WS-DEPOSITO
+               IF WS-DEPOSITO > 0 THEN
+                   COMPUTE WS-SALDO = WS-SALDO + WS-DEPOSITO
+                   DISPLAY "DEPOSITO DE R$ " WS-DEPOSITO " REALIZADO COM SUCESSO"
+                   MOVE WS-SALDO TO WS-SALDO-FORMATADO
+                   DISPLAY "NOVO SALDO: R$ " WS-SALDO-FORMATADO
                ELSE
-                   SUBTRACT VALOR FROM SALDO
-                   DISPLAY 'SAQUE REALIZADO COM SUCESSO. SEU NOVO SALDO E: ' SALDO
-               END-IF
-           ELSE
-               DISPLAY 'VALOR INVALIDO PARA SAQUE.'
-           END-IF.
+                   DISPLAY "VALOR DE DEPOSITO INVALIDO"
+               END-IF.
 
-         END PROGRAM CAIXA-ELETRONICO.
+           P003-SAQUE.
+               PERFORM P000-TITULO
+               DISPLAY "VALOR DO SAQUE: "
+               ACCEPT WS-SAQUE
+               IF WS-SAQUE > 0 THEN
+                   IF WS-SAQUE > WS-SALDO THEN
+                       DISPLAY "SALDO INSUFICIENTE"
+                   ELSE
+                       COMPUTE WS-SALDO = WS-SALDO - WS-SAQUE
+                       DISPLAY "SAQUE DE R$ " WS-SAQUE " REALIZADO COM SUCESSO"
+                       MOVE WS-SALDO TO WS-SALDO-FORMATADO
+                       DISPLAY "NOVO SALDO: R$ " WS-SALDO-FORMATADO
+                   END-IF
+               ELSE
+                   DISPLAY "VALOR DE SAQUE INVALIDO"
+               END-IF.
 
-         STOP RUN.
+           P004-SALDO.
+               PERFORM P000-TITULO
+               MOVE WS-SALDO TO WS-SALDO-FORMATADO
+               DISPLAY "SALDO ATUAL: R$ " WS-SALDO-FORMATADO.
